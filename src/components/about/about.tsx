c6424493e05659487743e7e540382734160280f6
Ivan Porollo <iporollo@gmail.com>
2020-08-26 19:29:48 -0700

    removing outdated info & adding kosmos

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,31 +1,29 @@
-import * as React from 'react';
-import PageContainer from '../page-container/page-container';
+import * as React from "react";
+import PageContainer from "../page-container/page-container";
 import TerminalImage from "../terminal-image/terminal-image";
 import Typist from "react-typist";
 
-import * as LinkedInLogo from '../../images/LinkedInLogo.png';
-import * as GithubLogo from '../../images/GithubLogo.png';
-import * as ResumeLogo from '../../images/ResumeLogo.png';
+import * as LinkedInLogo from "../../images/LinkedInLogo.png";
+import * as GithubLogo from "../../images/GithubLogo.png";
+import * as ResumeLogo from "../../images/ResumeLogo.png";
 
-import './about.css';
+import "./about.css";
 
 interface AboutStates {
   isAboutTerminalTextShown: boolean;
 }
 
 class About extends React.Component<any, AboutStates> {
-
   public constructor(props: any) {
     super(props);
     this.state = {
-      isAboutTerminalTextShown: false
-    }
+      isAboutTerminalTextShown: false,
+    };
     this.showAboutTerminalText = this.showAboutTerminalText.bind(this);
   }
 
   private showAboutTerminalText() {
     this.setState({ isAboutTerminalTextShown: true });
-
   }
 
   public render() {
@@ -33,7 +31,6 @@ class About extends React.Component<any, AboutStates> {
       <PageContainer>
         <div className="about-page">
           <div className="about-left">
-
             <div className="terminal_window">
               <header>
                 <div className="terminal-buttons">
@@ -47,63 +44,67 @@ class About extends React.Component<any, AboutStates> {
                 <div></div>
               </header>
               <div id="typed-strings" className="terminal_text">
-
                 <span className={"terminal-custom-typist"}>
                   Ivans-Portfolio: ~ $
-                    <Typist avgTypingDelay={40}
+                  <Typist
+                    avgTypingDelay={40}
                     startDelay={1500}
                     className={"terminal-custom-typist-div"}
                     cursor={{
                       show: true,
                       blink: true,
-                      element: '|',
+                      element: "|",
                       hideWhenDone: true,
                       hideWhenDoneDelay: 50,
                     }}
                     onTypingDone={this.showAboutTerminalText}
                   >
                     cat about_me.txt
-                    </Typist>
+                  </Typist>
                 </span>
                 {this.state.isAboutTerminalTextShown && [
                   <p key={"text1"}>
-                    Welcome! Thanks for checking out my portfolio website. I am recent grad from The Ohio State University and am currently employed at Weedmaps.
-                    </p>
-                  ,
-                  <p key={"text2"}>From a professional standpoint, I started freelancing during my senior year of college. I continued freelancing after graduation before joining as a front-end developer at Weedmaps. You can find out more about what I've done in the <span style={{ textDecoration: "underline", color: "#98FB98" }}>Experience</span> and <span style={{ textDecoration: "underline", color: "#98FB98" }}>Portfolio</span> tabs.
-                    </p>
-                  ,
-                  <p key={"text3"}>If you are interested in my freelance work and would like to contact me to discuss a project, please visit the <span style={{ textDecoration: "underline", color: "#98FB98" }}>Freelance</span> tab for more information.
-                    </p>
-                  ,
+                    Welcome! Thanks for checking out my portfolio website. I am
+                    currently building Kosmos.
+                  </p>,
                   <span className={"terminal-custom-typist"} key={"text4"}>
                     Ivans-Portfolio: ~ $
-                      <Typist avgTypingDelay={40}
+                    <Typist
+                      avgTypingDelay={40}
                       startDelay={500}
                       className={"terminal-custom-typist-div"}
                       cursor={{
                         show: true,
                         blink: true,
-                        element: '|',
+                        element: "|",
                         hideWhenDone: false,
-                      }}>
-                    </Typist>
-                  </span>
+                      }}
+                    ></Typist>
+                  </span>,
                 ]}
-
               </div>
               <span id="typed"></span>
             </div>
           </div>
           <div className="about-right">
-            <TerminalImage terminalName={"Resume"} terminalImage={ResumeLogo} terminalImageHeight={60} terminalImageWidth={60} terminalLink={"https://drive.google.com/file/d/1GAoChdYmpFGA-7u5OwNbwPOrlqK5dOCu/view?usp=sharing"} />
-            <TerminalImage terminalName={"LinkedIn"} terminalImage={LinkedInLogo} terminalImageHeight={50} terminalImageWidth={50} terminalLink={"https://www.linkedin.com/in/iporollo/"} />
-            <TerminalImage terminalName={"Github"} terminalImage={GithubLogo} terminalImageHeight={60} terminalImageWidth={60} terminalLink={"https://github.com/iporollo"} />
+            {/* <TerminalImage terminalName={"Resume"} terminalImage={ResumeLogo} terminalImageHeight={60} terminalImageWidth={60} terminalLink={"https://drive.google.com/file/d/1GAoChdYmpFGA-7u5OwNbwPOrlqK5dOCu/view?usp=sharing"} /> */}
+            <TerminalImage
+              terminalName={"LinkedIn"}
+              terminalImage={LinkedInLogo}
+              terminalImageHeight={50}
+              terminalImageWidth={50}
+              terminalLink={"https://www.linkedin.com/in/iporollo/"}
+            />
+            <TerminalImage
+              terminalName={"Github"}
+              terminalImage={GithubLogo}
+              terminalImageHeight={60}
+              terminalImageWidth={60}
+              terminalLink={"https://github.com/iporollo"}
+            />
           </div>
         </div>
-
       </PageContainer>
-
     );
   }
 }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,96 +1,103 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { Link } from "react-router-dom";
 import Typist from "react-typist";
 import { NavbarTabsEnum } from "../../util/NavbarTabsEnum";
 import ReactModal from "react-modal";
 import ModalCloseIcon from "../icons/modal-close-icon";
-import Dropdown, { MenuItem } from '@trendmicro/react-dropdown';
+import Dropdown, { MenuItem } from "@trendmicro/react-dropdown";
 
 import * as FREELANCE_LOGO from "../../images/FreelanceLogo.png";
 
-import './navbar.css';
-import '@trendmicro/react-buttons/dist/react-buttons.css';
-import '@trendmicro/react-dropdown/dist/react-dropdown.css';
+import "./navbar.css";
+import "@trendmicro/react-buttons/dist/react-buttons.css";
+import "@trendmicro/react-dropdown/dist/react-dropdown.css";
 
 interface NavbarProps {
-    selectedTab: NavbarTabsEnum;
-    handleNavbarTabChangeCallback: any;
+  selectedTab: NavbarTabsEnum;
+  handleNavbarTabChangeCallback: any;
 }
 
 interface NavbarStates {
-    isFreelanceModalOpen: boolean
+  isFreelanceModalOpen: boolean;
 }
 
 class Navbar extends React.Component<NavbarProps, NavbarStates> {
-    public constructor(props: NavbarProps) {
-        super(props);
-        this.state = {
-            isFreelanceModalOpen: false
-        }
-        this.handleTabChange = this.handleTabChange.bind(this);
-        this.handleFreelanceClick = this.handleFreelanceClick.bind(this);
-        this.handleCloseFreelanceModal = this.handleCloseFreelanceModal.bind(this);
+  public constructor(props: NavbarProps) {
+    super(props);
+    this.state = {
+      isFreelanceModalOpen: false,
+    };
+    this.handleTabChange = this.handleTabChange.bind(this);
+    this.handleFreelanceClick = this.handleFreelanceClick.bind(this);
+    this.handleCloseFreelanceModal = this.handleCloseFreelanceModal.bind(this);
+  }
+
+  private handleTabChange(e: any) {
+    switch (e.target.innerText) {
+      case "About": {
+        this.props.handleNavbarTabChangeCallback(NavbarTabsEnum.About);
+        break;
+      }
+      case "Experience": {
+        this.props.handleNavbarTabChangeCallback(NavbarTabsEnum.Experience);
+        break;
+      }
+      case "Portfolio": {
+        this.props.handleNavbarTabChangeCallback(NavbarTabsEnum.Portfolio);
+        break;
+      }
+      default: {
+        this.props.handleNavbarTabChangeCallback(NavbarTabsEnum.About);
+        break;
+      }
     }
+  }
 
-    private handleTabChange(e: any) {
-        switch (e.target.innerText) {
-            case "About": {
-                this.props.handleNavbarTabChangeCallback(NavbarTabsEnum.About)
-                break;
-            }
-            case "Experience": {
-                this.props.handleNavbarTabChangeCallback(NavbarTabsEnum.Experience)
-                break;
-            }
-            case "Portfolio": {
-                this.props.handleNavbarTabChangeCallback(NavbarTabsEnum.Portfolio)
-                break;
-            }
-            default: {
-                this.props.handleNavbarTabChangeCallback(NavbarTabsEnum.About)
-                break;
-            }
-        }
-    }
+  private handleFreelanceClick() {
+    this.setState({ isFreelanceModalOpen: true });
+    return false;
+  }
 
-    private handleFreelanceClick() {
-        this.setState({ isFreelanceModalOpen: true });
-        return false;
-    }
+  private handleCloseFreelanceModal() {
+    this.setState({ isFreelanceModalOpen: false });
+  }
 
-    private handleCloseFreelanceModal() {
-        this.setState({ isFreelanceModalOpen: false });
-
-    }
-
-    public render() {
-        return (
-            <React.Fragment>
-                <header className="navbar">
-                    <div className="left">
-                        <h1>
-                            <Typist avgTypingDelay={130}
-                                startDelay={100}
-                                className={"custom_typist"}
-                                cursor={{
-                                    show: true,
-                                    blink: true,
-                                    element: '|',
-                                    hideWhenDone: true,
-                                    hideWhenDoneDelay: 1700,
-                                }}>
-                                Ivan Porollo
-                    </Typist>
-                        </h1>
-                    </div>
-                    {/* need this to fade in */}
-                    <div className="right">
-                        <Link to="/"
-                            style={this.props.selectedTab === NavbarTabsEnum.About ? { textDecoration: "underline" } : {}}
-                            onClick={this.handleTabChange}>
-                            About
-                </Link>
-                        <Link to="/experience"
+  public render() {
+    return (
+      <React.Fragment>
+        <header className="navbar">
+          <div className="left">
+            <h1>
+              <Typist
+                avgTypingDelay={130}
+                startDelay={100}
+                className={"custom_typist"}
+                cursor={{
+                  show: true,
+                  blink: true,
+                  element: "|",
+                  hideWhenDone: true,
+                  hideWhenDoneDelay: 1700,
+                }}
+              >
+                Ivan Porollo
+              </Typist>
+            </h1>
+          </div>
+          {/* need this to fade in */}
+          <div className="right">
+            <Link
+              to="/"
+              style={
+                this.props.selectedTab === NavbarTabsEnum.About
+                  ? { textDecoration: "underline" }
+                  : {}
+              }
+              onClick={this.handleTabChange}
+            >
+              About
+            </Link>
+            {/* <Link to="/experience"
                             style={this.props.selectedTab === NavbarTabsEnum.Experience ? { textDecoration: "underline" } : {}}
                             onClick={this.handleTabChange}>
                             Experience
@@ -100,65 +107,117 @@ class Navbar extends React.Component<NavbarProps, NavbarStates> {
                             onClick={this.handleTabChange}>
                             Portfolio
                 </Link>
-                        <Link to="#" onClick={this.handleFreelanceClick} className="freelance-link">Freelance</Link>
-                    </div>
-                    <div className="dropdown-toggle-container">
-                        <Dropdown pullRight onSelect={(eventKey: any) => { }}>
-                            <Dropdown.Toggle iconOnly noCaret btnStyle="link" >
-                                <i className="fa fa-bars" id="dropdown-toggle-icon" />
-
-                            </Dropdown.Toggle>
-                            <Dropdown.Menu>
-                                <MenuItem active={this.props.selectedTab === NavbarTabsEnum.About ? true : false} onSelect={(eventKey: any) => { window.location.replace("/") }}>
-                                    About
-                                </MenuItem>
-                                <MenuItem active={this.props.selectedTab === NavbarTabsEnum.Experience ? true : false} onSelect={(eventKey: any) => { window.location.replace("/experience") }}>
-                                    Experience
-                                </MenuItem>
-                                <MenuItem active={this.props.selectedTab === NavbarTabsEnum.Portfolio ? true : false} onSelect={(eventKey: any) => { window.location.replace("/portfolio") }}>
-                                    Portfolio
-                                </MenuItem>
-                                <MenuItem onSelect={this.handleFreelanceClick}>
-                                    Freelance
-                                </MenuItem>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
-                </header>
-
-                <ReactModal isOpen={this.state.isFreelanceModalOpen}
-                    onRequestClose={this.handleCloseFreelanceModal}
-                    className="modal_container modal_container_simple"
-                    overlayClassName="c_overlay c_overlay_dark"
-                    closeTimeoutMS={200}
+                        <Link to="#" onClick={this.handleFreelanceClick} className="freelance-link">Freelance</Link> */}
+            <a href="https://usekosmos.com">Kosmos</a>
+          </div>
+          <div className="dropdown-toggle-container">
+            <Dropdown pullRight onSelect={(eventKey: any) => {}}>
+              <Dropdown.Toggle iconOnly noCaret btnStyle="link">
+                <i className="fa fa-bars" id="dropdown-toggle-icon" />
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <MenuItem
+                  active={
+                    this.props.selectedTab === NavbarTabsEnum.About
+                      ? true
+                      : false
+                  }
+                  onSelect={(eventKey: any) => {
+                    window.location.replace("/");
+                  }}
                 >
-                    <div className="modal modal_light modal_simple">
-                        <div className="modal_content">
-                            <div className="modal_x-close" onClick={this.handleCloseFreelanceModal}>
-                                <ModalCloseIcon fill={"rgba(0,0,0,0.5)"} />
-                            </div>
-                            <div className="modal_header modal_header_custom modal_header_background-filled">
-                                <div className="modal_header_decor"></div>
-                                <div className="modal_header_img_container" style={{ backgroundColor: "rgb(252, 209, 42)" }}>
-                                    <img src={FREELANCE_LOGO} height="40px" width="40px" className="modal_header_img"></img>
-                                </div>
-                            </div>
-                            <div className="modal_body">
-                                <div className="modal_body_description">
-                                    <p>I'm a web application developer with 3+ years of experience. I have worked with multiple companies and projects to create quality websites.</p>
-                                    <p>I can help with projects revolving around the creation of websites using React.js and many other front-end technologies (HTML, CSS, JQuery, Ajax).</p>
-                                    <p>I have also interfaced with and developed many RESTful APIs, using technologies such as Ruby on Rails, C#, and Python. </p>
-                                    <p>If you’re interested in contacting me about a project, please email me at:</p>
-                                    <p>iporollo(at)gmail(dot)com</p>
-                                </div>
-                            </div>
+                  About
+                </MenuItem>
+                <MenuItem
+                  active={
+                    this.props.selectedTab === NavbarTabsEnum.Experience
+                      ? true
+                      : false
+                  }
+                  onSelect={(eventKey: any) => {
+                    window.location.replace("/experience");
+                  }}
+                >
+                  Experience
+                </MenuItem>
+                <MenuItem
+                  active={
+                    this.props.selectedTab === NavbarTabsEnum.Portfolio
+                      ? true
+                      : false
+                  }
+                  onSelect={(eventKey: any) => {
+                    window.location.replace("/portfolio");
+                  }}
+                >
+                  Portfolio
+                </MenuItem>
+                <MenuItem onSelect={this.handleFreelanceClick}>
+                  Freelance
+                </MenuItem>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </header>
 
-                        </div>
-                    </div>
-                </ReactModal>
-            </React.Fragment>
-        );
-    }
+        <ReactModal
+          isOpen={this.state.isFreelanceModalOpen}
+          onRequestClose={this.handleCloseFreelanceModal}
+          className="modal_container modal_container_simple"
+          overlayClassName="c_overlay c_overlay_dark"
+          closeTimeoutMS={200}
+        >
+          <div className="modal modal_light modal_simple">
+            <div className="modal_content">
+              <div
+                className="modal_x-close"
+                onClick={this.handleCloseFreelanceModal}
+              >
+                <ModalCloseIcon fill={"rgba(0,0,0,0.5)"} />
+              </div>
+              <div className="modal_header modal_header_custom modal_header_background-filled">
+                <div className="modal_header_decor"></div>
+                <div
+                  className="modal_header_img_container"
+                  style={{ backgroundColor: "rgb(252, 209, 42)" }}
+                >
+                  <img
+                    src={FREELANCE_LOGO}
+                    height="40px"
+                    width="40px"
+                    className="modal_header_img"
+                  ></img>
+                </div>
+              </div>
+              <div className="modal_body">
+                <div className="modal_body_description">
+                  <p>
+                    I'm a web application developer with 3+ years of experience.
+                    I have worked with multiple companies and projects to create
+                    quality websites.
+                  </p>
+                  <p>
+                    I can help with projects revolving around the creation of
+                    websites using React.js and many other front-end
+                    technologies (HTML, CSS, JQuery, Ajax).
+                  </p>
+                  <p>
+                    I have also interfaced with and developed many RESTful APIs,
+                    using technologies such as Ruby on Rails, C#, and Python.{" "}
+                  </p>
+                  <p>
+                    If you’re interested in contacting me about a project,
+                    please email me at:
+                  </p>
+                  <p>iporollo(at)gmail(dot)com</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </ReactModal>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Navbar;
